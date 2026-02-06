dc6303eaae4d6f4ade35283dea3b79b91fffdbb9
马晓川 <724503670@qq.com>
2026-02-06 18:43:37 +0800

    feat: 添加 commitlint 配置并导出共享组件

--- packages/shared/src/components/index.ts
+++ packages/shared/src/components/index.ts
@@ -2,7 +2,3 @@
 export { default as BaseButton } from './BaseButton.vue'
 export { default as BaseCard } from './BaseCard.vue'
 export { default as LoadingOverlay } from './LoadingOverlay.vue'
-
-// 类型导出
-export type { BaseButtonProps } from './BaseButton.vue'
-export type { BaseCardProps } from './BaseCard.vue'
