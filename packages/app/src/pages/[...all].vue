a661069883bc2a0c66df8fb3b02f346a6ade16cc
马晓川 <724503670@qq.com>
2026-02-09 11:49:51 +0800

    feat: 添加 404 未找到页面。

--- packages/app/src/pages/[...all].vue
+++ packages/app/src/pages/[...all].vue
@@ -10,15 +10,23 @@ function onBack() {
 </script>
 
 <template>
-  <div text="center gray-300 dark:gray-200">
-    <van-icon name="warn-o" size="3em" />
-    <div> Not found </div>
+  <div class="px-4 bg-gray-50 flex flex-col min-h-screen items-center justify-center dark:bg-gray-950">
+    <div class="i-carbon-warning-alt text-6xl text-orange-500 mb-6 animate-bounce" />
 
-    <div class="mt-2">
-      <button van-haptics-feedback class="btn" @click="onBack">
-        Back
-      </button>
-    </div>
+    <h1 class="text-3xl text-gray-900 font-bold mb-2 dark:text-white">
+      404 Not Found
+    </h1>
+
+    <p class="text-gray-500 mb-8 text-center max-w-xs dark:text-gray-400">
+      Oops! The page you are looking for doesn't exist or has been moved.
+    </p>
+
+    <button
+      class="text-white font-medium px-6 py-2.5 rounded-xl bg-indigo-500 shadow-indigo-500/30 shadow-lg transition-transform hover:bg-indigo-600 active:scale-95"
+      @click="onBack"
+    >
+      Go Back Home
+    </button>
   </div>
 </template>
 
